ea0419da8bce4d452440b8692e15ddf8840800a9
Lauveno <loadtume5749@gmail.com>
2021-10-27 01:49:17 +0900

    WWG WEB Tap 연결

--- rootWEB/mapApp/urls.py
+++ rootWEB/mapApp/urls.py
@@ -5,5 +5,15 @@ from mapApp import views
 
 urlpatterns = [
     # http://127.0.0.1:8000/map/main
-    path('main/' , views.index) ,
+    path('main/' , views.index , name = "main") ,
+    #user
+    path('login/' , views.login , name = "login") ,
+    path('logout/' , views.logout , name = "logout") ,
+    path('join/' , views.join , name = "join") ,
+    path('registerForm/' , views.registerForm , name = "registerForm") ,
+    # news
+    path('news/' , views.news , name = "news") ,
+    # path('news_read/' , views.news_read , name = "news_read") ,
+    # register
+    path('register/' , views.register , name = "register") ,
 ]
--- rootWEB/mapApp/views.py
+++ rootWEB/mapApp/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 
 def index(request) :
     print('mapApp index ~ ')
     return render(request , 'map/index.html')
+
+def login(request) :
+    print('mapApp index ~ ')
+    return render(request , 'map/login.html')
+
+def logout(request) :
+    return redirect('main')
+
+def join(request) :
+    return redirect('main')
+
+def registerForm(request) :
+    return render(request , 'map/join.html')
+
+
+def news(request) :
+    print('mapApp index ~ ')
+    return render(request , 'map/news.html')
+
+
+def register(request) :
+    print('mapApp index ~ ')
+    return render(request , 'map/register.html')
